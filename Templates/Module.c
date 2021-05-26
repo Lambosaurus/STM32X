44795341d4d1a08690429ca476a0cdb3ff0bfbfc
Lambosaurus <tlamborn@gmail.com>
2021-05-26 12:35:28 +1200

    Added module templates

---
+++ Templates/Module.c
@@ -0,0 +1,35 @@
+
+#include "Module.h"
+
+/*
+ * PRIVATE DEFINITIONS
+ */
+
+/*
+ * PRIVATE TYPES
+ */
+
+/*
+ * PRIVATE PROTOTYPES
+ */
+
+/*
+ * PRIVATE VARIABLES
+ */
+
+/*
+ * PUBLIC FUNCTIONS
+ */
+
+void Module_Init(void)
+{
+}
+
+/*
+ * PRIVATE FUNCTIONS
+ */
+
+/*
+ * INTERRUPT ROUTINES
+ */
+
---
+++ Templates/Module.h
@@ -0,0 +1,30 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+#include "STM32X.h"
+
+/*
+ * FUNCTIONAL TESTING
+ * STM32L0: N
+ * STM32F0: N
+ */
+
+/*
+ * PUBLIC DEFINITIONS
+ */
+
+/*
+ * PUBLIC TYPES
+ */
+
+/*
+ * PUBLIC FUNCTIONS
+ */
+
+void Module_Init(void);
+
+/*
+ * EXTERN DECLARATIONS
+ */
+
+#endif //MODULE_H
