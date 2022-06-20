f93fe64a7b2b972ea6ffe5d67ba9ca6a1804840f
Tim Lamborn <tlamborn@gmail.com>
2022-06-20 14:01:48 +1200

    Set debug modes for STM32Wl

--- Lib/Core.c
+++ Lib/Core.c
@@ -87,6 +87,11 @@ void CORE_Init(void)
 	__HAL_PWR_VOLTAGESCALING_CONFIG(CORE_VOLTAGE_RANGE);
 #endif
 
+#if defined(DEBUG) && (defined(STM32WL) || defined(STM32G0))
+	HAL_DBGMCU_EnableDBGSleepMode();
+	HAL_DBGMCU_EnableDBGStopMode();
+#endif
+
 	CLK_InitSYSCLK();
 	CORE_InitSysTick();
 	CORE_InitGPIO();
