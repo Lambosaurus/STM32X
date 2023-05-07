4941f9950cceccb130b6582090fa58587aa038c8
Tim Lamborn <tlamborn@gmail.com>
2023-05-07 19:41:04 +1200

    Get TSC working

--- Lib/TSC.c
+++ Lib/TSC.c
@@ -10,6 +10,10 @@
  * PRIVATE DEFINITIONS
  */
 
+#define TSC_FREQ_TARGET		1000000
+#define TSC_CHARGE_CYCLES	4
+#define TSC_CYCLES_MAX		TSC_MCV_8191
+
 /*
  * PRIVATE TYPES
  */
@@ -26,22 +30,23 @@
  * PUBLIC FUNCTIONS
  */
 
-void TSC_Init(uint32_t freq)
+void TSC_Init(void)
 {
-	//uint32_t div = CLK_SelectDivider(freq);
 	uint32_t src_freq = CLK_GetHCLKFreq();
+	uint32_t freq = TSC_FREQ_TARGET;
 	uint32_t div = CLK_SelectPrescalar(src_freq, 1, 128, &freq);
 
 	__HAL_RCC_TSC_CLK_ENABLE();
 
 	TSC->CR = TSC_CR_TSCE;
 
-	TSC->CR |= (TSC_CTPH_10CYCLES
-			 | TSC_CTPL_10CYCLES
+	TSC->CR |= (
+			   (TSC_CTPH_2CYCLES * (TSC_CHARGE_CYCLES - 1))
+			 | (TSC_CTPL_2CYCLES * (TSC_CHARGE_CYCLES - 1))
 			 | (TSC_CTPL_1CYCLE << TSC_CR_SSD_Pos)
 			 | TSC_SS_PRESC_DIV1
 			 | (div * TSC_CR_PGPSC_0)
-			 | TSC_MCV_4095
+			 | TSC_CYCLES_MAX
 			 | TSC_SYNC_POLARITY_FALLING
 			 | TSC_ACQ_MODE_NORMAL
 			 // | TSC_CR_SSE
@@ -62,9 +67,12 @@ void TSC_Deinit(void)
 
 void TSC_EnableChannel(TSC_Group_t group, TSC_Channel_t ch, GPIO_Pin_t pin)
 {
-	GPIO_EnableAlternate(pin, TSC_AF, GPIO_Flag_None);
+	// This function can enable multiple channels at once, so long as:
+	//  - They are on the same group
+	//  - They are on the same GPIO bank
+	GPIO_EnableAlternate(pin, GPIO_Flag_None, TSC_AF);
 
-	uint32_t tsc_ch = (group << 2) | ch;
+	uint32_t tsc_ch = ch << (group << 2);
 
 	TSC->IOHCR &= ~tsc_ch;
 	TSC->IOCCR |= tsc_ch;
@@ -72,9 +80,9 @@ void TSC_EnableChannel(TSC_Group_t group, TSC_Channel_t ch, GPIO_Pin_t pin)
 
 void TSC_EnableGroup(TSC_Group_t group, TSC_Channel_t sample_ch, GPIO_Pin_t sample_pin)
 {
-	GPIO_EnableAlternate(sample_pin, TSC_AF, GPIO_Flag_OpenDrain);
+	GPIO_EnableAlternate(sample_pin, GPIO_Flag_OpenDrain, TSC_AF);
 
-	uint32_t tsc_ch = (group << 2) | sample_ch;
+	uint32_t tsc_ch = sample_ch << (group << 2);
 
 	TSC->IOHCR &= ~tsc_ch;
 	TSC->IOSCR |= tsc_ch;
@@ -83,14 +91,8 @@ void TSC_EnableGroup(TSC_Group_t group, TSC_Channel_t sample_ch, GPIO_Pin_t samp
 
 void TSC_Sample(void)
 {
-	// Enable IO grounding (Required to discharge capacitor).
-	//TSC->CR &= ~TSC_CR_IODEF;
-	//CORE_Delay(100);
-	// Disable IO grounding
-	//TSC->CR |= TSC_CR_IODEF;
-
 	// Clear end of acquisition flag
-	TSC->ICR |= TSC_FLAG_EOA;
+	TSC->ICR |= TSC_FLAG_EOA | TSC_FLAG_MCE;
 
 	// Start acquisition and wait for end
 	TSC->CR |= TSC_CR_START;
