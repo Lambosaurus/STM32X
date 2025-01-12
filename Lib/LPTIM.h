3004e87fdd523556e3558b379a9134106cf6c38a
Tim Lamborn <tlamborn@gmail.com>
2025-01-13 12:32:10 +1300

    Implement basic LPTIM1 reading

---
+++ Lib/LPTIM.h
@@ -0,0 +1,44 @@
+#ifndef LPTIM_H
+#define LPTIM_H
+
+#include "STM32X.h"
+
+#ifdef LPTIM_ENABLE
+
+/*
+ * FUNCTIONAL TESTING
+ * STM32L0: Y
+ * STM32F0: N
+ * STM32G0: N
+ * STM32WL: N
+ */
+
+/*
+ * PUBLIC DEFINITIONS
+ */
+
+/*
+ * PUBLIC TYPES
+ */
+
+/*
+ * PUBLIC FUNCTIONS
+ */
+
+// Initialisation
+void LPTIM_Init(uint32_t freq, uint32_t reload);
+void LPTIM_Deinit();
+void LPTIM_SetFreq(uint32_t freq);
+void LPTIM_SetReload(uint32_t reload);
+
+// Base counter features
+void LPTIM_Start(void);
+void LPTIM_Stop(void);
+uint32_t LPTIM_Read(void);
+
+/*
+ * EXTERN DECLARATIONS
+ */
+
+#endif //LPTIM_ENABLE
+#endif //LPTIM_H
