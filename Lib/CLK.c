a3b94cccfdbdc4fa8def3f666179614be5bcda51
Tim Lamborn <tlamborn@gmail.com>
2025-05-11 16:37:33 +1200

    Fix inversion with RTC BDCR

--- Lib/CLK.c
+++ Lib/CLK.c
@@ -329,15 +329,15 @@ static void CLK_ResetBackupDomain(void)
 {
 	// RTC Clock selection can be changed only if the Backup Domain is reset
 #if defined(STM32G0)
-	uint32_t csr = (RCC->CSR & ~(RCC_CSR_RTCSEL));
-	__HAL_RCC_BACKUPRESET_FORCE();
-	__HAL_RCC_BACKUPRESET_RELEASE();
-	RCC->CSR = csr;
-#else
 	uint32_t bdcr = (RCC->BDCR & ~(RCC_BDCR_RTCSEL));
 	__HAL_RCC_BACKUPRESET_FORCE();
 	__HAL_RCC_BACKUPRESET_RELEASE();
 	RCC->BDCR = bdcr;
+#else
+	uint32_t csr = (RCC->CSR & ~(RCC_CSR_RTCSEL));
+	__HAL_RCC_BACKUPRESET_FORCE();
+	__HAL_RCC_BACKUPRESET_RELEASE();
+	RCC->CSR = csr;
 #endif
 }
 #endif
